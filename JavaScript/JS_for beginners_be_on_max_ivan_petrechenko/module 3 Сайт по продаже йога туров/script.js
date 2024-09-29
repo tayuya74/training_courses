4a622b9632839af924537495e783283590f4ab0d
tayuya74 <lyna-93@mail.ru>
2024-09-30 03:04:21 +0500

    add form

--- JavaScript/JS_for beginners_be_on_max_ivan_petrechenko/module 3 Сайт по продаже йога туров/script.js
+++ JavaScript/JS_for beginners_be_on_max_ivan_petrechenko/module 3 Сайт по продаже йога туров/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
   })
 
   //timer
-  let deadline = '2024-06-16'
+  let deadline = '2024-08-16'
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
@@ -102,6 +102,52 @@ function modalWindow() {
 }
 modalWindow()
 
+// form
+
+let message = {
+  loading: 'load',
+  success: 'thx',
+  failure: 'fail'
+}
+
+let form = document.querySelector('.main-form')
+let input = form.getElementsByTagName('input')
+let statusMessage = document.createElement('div')
+statusMessage.classList.add('status')
+
+form.addEventListener('submit', function(event) {
+   event.preventDefault()
+   form.appendChild(statusMessage)
+
+   let request = new XMLHttpRequest();
+   request.open('POST', 'server.php');
+   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+   //получаем данные от пользователя
+   let formData = new FormData(form)
+
+   let obj = {}
+   formData.forEach(function(value, key) {
+    obj[key] = value
+   })
+   let json = JSON.stringify(obj)
+
+   request.send(json)
+
+   request.addEventListener('readystatechange', function(){
+    if (request.readyState < 4) {
+       statusMessage.innerHTML = message.loading
+    } else if (request.readyState === 4 && request.status == 200) {
+      statusMessage.innerHTML = message.success
+    } else {
+      statusMessage.innerHTML = message.failure
+    }
+   })
+
+   for (let i = 0; i < input.length; i++) {
+    input[i].value = ''
+   }
+  })
 
 })
 
