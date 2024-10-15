f3fcddf2439fac6dd47536d9f13b3e574b35e5ff
tayuya74 <lyna-93@mail.ru>
2024-10-15 20:22:35 +0500

    lesson 4.6 add slider

--- JavaScript/JS_for beginners_be_on_max_ivan_petrechenko/module 3 Сайт по продаже йога туров/script.js
+++ JavaScript/JS_for beginners_be_on_max_ivan_petrechenko/module 3 Сайт по продаже йога туров/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
   })
 
   //timer
-  let deadline = '2024-08-16'
+  let deadline = '2024-10-16'
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
@@ -81,73 +81,124 @@ window.addEventListener('DOMContentLoaded', function() {
 
   setClock('timer', deadline)
 
-//modal window
+  //modal window
 
-function modalWindow() {
-  let more = document.querySelector('.more')
-  let overlay = document.querySelector('.overlay')
-  let close = document.querySelector('.popup-close')
+  function modalWindow() {
+    let more = document.querySelector('.more')
+    let overlay = document.querySelector('.overlay')
+    let close = document.querySelector('.popup-close')
 
-  more.addEventListener('click' , function() {
-    overlay.style.display = 'block'
-    this.classList.add('more-splash')
-    document.body.style.overflow = 'hidden'
-  })
+    more.addEventListener('click' , function() {
+      overlay.style.display = 'block'
+      this.classList.add('more-splash')
+      document.body.style.overflow = 'hidden'
+    })
 
-  close.addEventListener('click', function(){
-    overlay.style.display = 'none'
-    more.classList.remove('more-splash')
-    document.body.style.overflow = ''
-  })
-}
-modalWindow()
+    close.addEventListener('click', function(){
+      overlay.style.display = 'none'
+      more.classList.remove('more-splash')
+      document.body.style.overflow = ''
+    })
+  }
+  modalWindow()
 
-// form
+  // form
 
-let message = {
-  loading: 'load',
-  success: 'thx',
-  failure: 'fail'
-}
+  let message = {
+    loading: 'load',
+    success: 'thx',
+    failure: 'fail'
+  }
 
-let form = document.querySelector('.main-form')
-let input = form.getElementsByTagName('input')
-let statusMessage = document.createElement('div')
-statusMessage.classList.add('status')
+  let form = document.querySelector('.main-form')
+  let input = form.getElementsByTagName('input')
+  let statusMessage = document.createElement('div')
+  statusMessage.classList.add('status')
 
-form.addEventListener('submit', function(event) {
-   event.preventDefault()
-   form.appendChild(statusMessage)
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    form.appendChild(statusMessage)
 
-   let request = new XMLHttpRequest();
-   request.open('POST', 'server.php');
-   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-   //получаем данные от пользователя
-   let formData = new FormData(form)
+    //получаем данные от пользователя
+    let formData = new FormData(form)
 
-   let obj = {}
-   formData.forEach(function(value, key) {
-    obj[key] = value
-   })
-   let json = JSON.stringify(obj)
+    let obj = {}
+    formData.forEach(function(value, key) {
+      obj[key] = value
+    })
+    let json = JSON.stringify(obj)
 
-   request.send(json)
+    request.send(json)
 
-   request.addEventListener('readystatechange', function(){
-    if (request.readyState < 4) {
-       statusMessage.innerHTML = message.loading
-    } else if (request.readyState === 4 && request.status == 200) {
-      statusMessage.innerHTML = message.success
-    } else {
-      statusMessage.innerHTML = message.failure
+    request.addEventListener('readystatechange', function(){
+      if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading
+      } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success
+      } else {
+        statusMessage.innerHTML = message.failure
+      }
+    })
+
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = ''
     }
-   })
+  })
 
-   for (let i = 0; i < input.length; i++) {
-    input[i].value = ''
-   }
+  //slider
+
+  let slideIndex = 1
+  let slides = document.querySelectorAll('.slider-item')
+  let prev = document.querySelector('.prev')
+  let next = document.querySelector('.next')
+  let dotsWrap = document.querySelector('.slider-dots')
+  let dots = document.querySelectorAll('.dot')
+
+  showSlides(slideIndex)
+
+  function showSlides(n) {
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+
+    slides.forEach((item) => item.style.display = 'none')
+    dots.forEach((item) => item.classList.remove('dot-active'))
+    slides[slideIndex - 1].style.display = 'block'
+    dots[slideIndex - 1].classList.add('dot-active')
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex +=n)
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n)
+  }
+
+  //управление слайдером через стрелки
+  prev.addEventListener('click', function() {
+    plusSlides(-1)
+  })
+
+  next.addEventListener('click', function() {
+    plusSlides(1)
+  })
+
+  //управление слайдером через "точки"
+  dotsWrap.addEventListener('click', function(event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+        currentSlide(i)
+      }
+    }
   })
 
 })
-
